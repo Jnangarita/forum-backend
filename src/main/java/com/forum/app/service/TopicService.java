a028ca9e84369c3e3a21bd95d01356ff2e9a0930
John Angarita <jnangaritaortiz@gmail.com>
2024-01-30 21:04:18 -0500

    refactor: Create impl package
    Created the 'impl' package to organize the implementation of services.
    
    In this package, the TopicServiceImpl class has been added, which implements
    the methods defined in the TopicService interface.
    
    Modified the definition of the TopicService class to reflect the change
    from a class to an interface.
    
    Refs: TRELLO #16

--- src/main/java/com/forum/app/service/TopicService.java
+++ src/main/java/com/forum/app/service/TopicService.java
@@ -1,64 +1,14 @@
 package com.forum.app.service;
 
-import java.time.LocalDateTime;
-
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.forum.app.dto.SaveTopicDTO;
 import com.forum.app.dto.TopicDTO;
 import com.forum.app.dto.UpdateTopicDTO;
 import com.forum.app.entity.Topic;
-import com.forum.app.exception.OwnRuntimeException;
-import com.forum.app.repository.TopicRepository;
 
-@Service
-public class TopicService {
-	@Autowired
-	private TopicRepository topicRepository;
+public interface TopicService {
+	Topic createTopic(SaveTopicDTO payload);
 
-	@Transactional
-	public Topic createTopic(SaveTopicDTO payload) {
-		try {
-			Topic newTopic = new Topic();
-			LocalDateTime currentDate = LocalDateTime.now();
-			newTopic.setIdCategory(payload.getIdCategory());
-			newTopic.setQuestion(payload.getQuestion());
-			newTopic.setIdUser(payload.getIdUser());
-			newTopic.setCreationDate(currentDate);
-			newTopic.setDeleted(false);
-			return topicRepository.save(newTopic);
-		} catch (Exception e) {
-			throw new OwnRuntimeException("Error Creating New Question");
-		}
-	}
+	TopicDTO getTopic(Long id);
 
-	public TopicDTO getTopic(Long id) {
-		Topic topic = topicRepository.getReferenceById(id);
-		Long idCategory = topic.getIdCategory();
-		String question = topic.getQuestion();
-		Long idUser = topic.getIdUser();
-		LocalDateTime creationDate = topic.getCreationDate();
-		boolean deleted = topic.isDeleted();
-		return new TopicDTO(id, idCategory, question, idUser, creationDate, deleted);
-	}
-
-	@Transactional
-	public TopicDTO updateTopic(UpdateTopicDTO payload) {
-		try {
-			Topic topic = topicRepository.getReferenceById(payload.getIdQuestion());
-			topic.setIdCategory(payload.getIdCategory());
-			topic.setQuestion(payload.getQuestion());
-			Long idCategory = topic.getIdCategory();
-			String question = topic.getQuestion();
-			Long idUser = topic.getIdUser();
-			LocalDateTime creationDate = topic.getCreationDate();
-			boolean deleted = topic.isDeleted();
-			return new TopicDTO(payload.getIdQuestion(), idCategory, question, idUser, creationDate, deleted);
-		} catch (Exception e) {
-			throw new OwnRuntimeException("Error Updating Question");
-		}
-	}
+	TopicDTO updateTopic(UpdateTopicDTO payload);
 }
---
+++ src/main/java/com/forum/app/service/impl/TopicServiceImpl.java
@@ -0,0 +1,68 @@
+package com.forum.app.service.impl;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.forum.app.dto.SaveTopicDTO;
+import com.forum.app.dto.TopicDTO;
+import com.forum.app.dto.UpdateTopicDTO;
+import com.forum.app.entity.Topic;
+import com.forum.app.exception.OwnRuntimeException;
+import com.forum.app.repository.TopicRepository;
+import com.forum.app.service.TopicService;
+
+@Service
+public class TopicServiceImpl implements TopicService {
+	@Autowired
+	private TopicRepository topicRepository;
+
+	@Transactional
+	@Override
+	public Topic createTopic(SaveTopicDTO payload) {
+		try {
+			Topic newTopic = new Topic();
+			LocalDateTime currentDate = LocalDateTime.now();
+			newTopic.setIdCategory(payload.getIdCategory());
+			newTopic.setQuestion(payload.getQuestion());
+			newTopic.setIdUser(payload.getIdUser());
+			newTopic.setCreationDate(currentDate);
+			newTopic.setDeleted(false);
+			return topicRepository.save(newTopic);
+		} catch (Exception e) {
+			throw new OwnRuntimeException("Error Creating New Question");
+		}
+	}
+
+	@Override
+	public TopicDTO getTopic(Long id) {
+		Topic topic = topicRepository.getReferenceById(id);
+		Long idCategory = topic.getIdCategory();
+		String question = topic.getQuestion();
+		Long idUser = topic.getIdUser();
+		LocalDateTime creationDate = topic.getCreationDate();
+		boolean deleted = topic.isDeleted();
+		return new TopicDTO(id, idCategory, question, idUser, creationDate, deleted);
+	}
+
+	@Transactional
+	@Override
+	public TopicDTO updateTopic(UpdateTopicDTO payload) {
+		try {
+			Topic topic = topicRepository.getReferenceById(payload.getIdQuestion());
+			topic.setIdCategory(payload.getIdCategory());
+			topic.setQuestion(payload.getQuestion());
+			Long idCategory = topic.getIdCategory();
+			String question = topic.getQuestion();
+			Long idUser = topic.getIdUser();
+			LocalDateTime creationDate = topic.getCreationDate();
+			boolean deleted = topic.isDeleted();
+			return new TopicDTO(payload.getIdQuestion(), idCategory, question, idUser, creationDate, deleted);
+		} catch (Exception e) {
+			throw new OwnRuntimeException("Error Updating Question");
+		}
+	}
+}
