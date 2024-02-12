4aa90c68a510974b36ca742a725cce49aa34cdbe
John Angarita <jnangaritaortiz@gmail.com>
2024-02-11 20:56:37 -0500

    refactor: Try FK exception
    Create the handleDataIntegrityViolation method in the TopicExceptionHandler class
    to handle foreign key exception
    
    Refs: TRELLO: #25

--- src/main/java/com/forum/app/exception/TopicExceptionHandler.java
+++ src/main/java/com/forum/app/exception/TopicExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.forum.app.exception;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,9 +18,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.forum.app.dto.BadRequestDTO;
 import com.forum.app.dto.ErrorDTO;
+import com.forum.app.dto.GeneralErrorDTO;
 
 @RestControllerAdvice
 public class TopicExceptionHandler {
+	@Autowired
+	private MessageSource messageSource;
+
+	Locale locale = LocaleContextHolder.getLocale();
+
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Void> entityNotFound(EntityNotFoundException e) {
 		return ResponseEntity.notFound().build();
@@ -24,9 +35,17 @@ public class TopicExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<BadRequestDTO> emptyOrNullField(MethodArgumentNotValidException e) {
 		Integer code = HttpStatus.BAD_REQUEST.value();
-		String message = "The payload is incorrect and can't be processed by the server";
+		String message = messageSource.getMessage("forum.message.error.empty.null.field", null, locale);
 		List<ErrorDTO> error = e.getFieldErrors().stream().map(ErrorDTO::new).collect(Collectors.toList());
 		BadRequestDTO badRequest = new BadRequestDTO(code, message, error);
 		return ResponseEntity.badRequest().body(badRequest);
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<GeneralErrorDTO> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+		String message = messageSource.getMessage("forum.message.error.foreign.key.constraint", null, locale);
+		String description = e.getMostSpecificCause().getMessage();
+		GeneralErrorDTO errorDTO = new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(), message, description);
+		return ResponseEntity.badRequest().body(errorDTO);
 	}
 }
--- src/main/java/com/forum/app/service/impl/TopicServiceImpl.java
+++ src/main/java/com/forum/app/service/impl/TopicServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Locale;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,6 @@ import com.forum.app.dto.TopicResponseDTO;
 import com.forum.app.dto.UpdateTopicDTO;
 import com.forum.app.entity.Topic;
 import com.forum.app.enumeration.QuestionStatus;
-import com.forum.app.exception.OwnRuntimeException;
 import com.forum.app.repository.TopicRepository;
 import com.forum.app.service.TopicService;
 
@@ -26,9 +24,6 @@ public class TopicServiceImpl implements TopicService {
 	@Autowired
 	private TopicRepository topicRepository;
 
-	@Autowired
-	private MessageSource messageSource;
-
 	LocalDateTime currentDate = LocalDateTime.now();
 
 	Locale locale = LocaleContextHolder.getLocale();
@@ -36,19 +31,15 @@ public class TopicServiceImpl implements TopicService {
 	@Transactional
 	@Override
 	public TopicResponseDTO createTopic(SaveTopicDTO payload) {
-		try {
-			Topic newTopic = new Topic();
-			newTopic.setIdCategory(payload.getIdCategory());
-			newTopic.setQuestion(payload.getQuestion());
-			newTopic.setIdUser(payload.getIdUser());
-			newTopic.setQuestionStatus(QuestionStatus.UNRESOLVED.getStatus());
-			newTopic.setCreationDate(currentDate);
-			newTopic.setDeleted(false);
-			Topic topic = topicRepository.save(newTopic);
-			return new TopicResponseDTO(topic);
-		} catch (Exception e) {
-			throw new OwnRuntimeException(messageSource.getMessage("forum.message.error.new.question", null, locale) + e);
-		}
+		Topic newTopic = new Topic();
+		newTopic.setIdCategory(payload.getIdCategory());
+		newTopic.setQuestion(payload.getQuestion());
+		newTopic.setIdUser(payload.getIdUser());
+		newTopic.setQuestionStatus(QuestionStatus.UNRESOLVED.getStatus());
+		newTopic.setCreationDate(currentDate);
+		newTopic.setDeleted(false);
+		Topic topic = topicRepository.save(newTopic);
+		return new TopicResponseDTO(topic);
 	}
 
 	@Override
@@ -60,15 +51,11 @@ public class TopicServiceImpl implements TopicService {
 	@Transactional
 	@Override
 	public TopicResponseDTO updateTopic(UpdateTopicDTO payload) {
-		try {
-			Topic topic = topicRepository.getReferenceById(payload.getIdQuestion());
-			topic.setIdCategory(payload.getIdCategory());
-			topic.setQuestion(payload.getQuestion());
-			topic.setModificationDate(currentDate);
-			return new TopicResponseDTO(topic);
-		} catch (Exception e) {
-			throw new OwnRuntimeException(messageSource.getMessage("forum.message.error.update.question", null, locale) + e);
-		}
+		Topic topic = topicRepository.getReferenceById(payload.getIdQuestion());
+		topic.setIdCategory(payload.getIdCategory());
+		topic.setQuestion(payload.getQuestion());
+		topic.setModificationDate(currentDate);
+		return new TopicResponseDTO(topic);
 	}
 
 	@Override
