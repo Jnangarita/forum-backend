e44c0fbdc354ebc9430889692dbed7ac1e9a65ce
John Angarita <jnangaritaortiz@gmail.com>
2024-06-12 10:56:53 -0500

    refactor: table documento

--- src/main/resources/db/migration/V7__CREATE-TABLE-DOCUMENTO.sql
+++ src/main/resources/db/migration/V7__CREATE-TABLE-DOCUMENTO.sql
@@ -1,6 +1,5 @@
 CREATE TABLE documento (
     id_documento INT AUTO_INCREMENT PRIMARY KEY,
-    codigo_usuario VARCHAR(10) NOT NULL,
     tipo_documento VARCHAR(50),
 	nombre_documento VARCHAR(50) NOT NULL,
 	ruta_documento VARCHAR(255) NOT NULL,
