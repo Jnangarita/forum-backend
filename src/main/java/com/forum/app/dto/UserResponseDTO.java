acb5d5da5df655b3668648d7a3b9bf239261164a
John Angarita <jnangaritaortiz@gmail.com>
2024-07-30 18:31:35 -0500

    refactor: add the new fields to the api response
    - add the new fields to the api response that creates
      a new user /api/forum/v1/users

--- src/main/java/com/forum/app/dto/UserResponseDTO.java
+++ src/main/java/com/forum/app/dto/UserResponseDTO.java
@@ -10,17 +10,29 @@ import lombok.Setter;
 @Setter
 @Getter
 public class UserResponseDTO {
-	private Long id;
 	private String code;
-	private String userName;
+	private String country;
 	private String email;
+	private Long id;
+	private Integer numberQuestions;
+	private Integer numberResponses;
+	private String photo;
+	private boolean deleted;
+	private String userName;
+	private Integer userRole;
 	private LocalDateTime createdAt;
 
 	public UserResponseDTO(User user) {
-		this.id = user.getId();
 		this.code = user.getCode();
-		this.userName = user.getFirstName() + " " + user.getLastName();
+		this.country = user.getCountry();
 		this.email = user.getEmail();
+		this.id = user.getId();
+		this.numberQuestions = user.getNumberQuestions();
+		this.numberResponses = user.getNumberResponses();
+		this.photo = user.getPhoto();
+		this.deleted = user.isDeleted();
+		this.userName = user.getFirstName() + " " + user.getLastName();
+		this.userRole = user.getRole();
 		this.createdAt = user.getCreatedAt();
 	}
 }
