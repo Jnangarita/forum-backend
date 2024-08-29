7540090929756b850f5b95d62913b495862b1c6f
John Angarita <jnangaritaortiz@gmail.com>
2024-08-29 11:30:45 -0500

    refactor: ForumExceptionHandler class

--- src/main/java/com/forum/app/exception/ForumExceptionHandler.java
+++ src/main/java/com/forum/app/exception/ForumExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.forum.app.exception;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
@@ -8,6 +7,7 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,63 +37,55 @@ public class ForumExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<BadRequestDTO> emptyOrNullField(MethodArgumentNotValidException e) {
-		Integer code = HttpStatus.BAD_REQUEST.value();
-		String message = utility.getMessage("forum.message.error.empty.null.field", null);
-		List<ErrorDTO> error = e.getFieldErrors().stream().map(ErrorDTO::new).collect(Collectors.toList());
-		BadRequestDTO badRequest = new BadRequestDTO(code, message, error);
-		return ResponseEntity.badRequest().body(badRequest);
+		return ResponseEntity.badRequest()
+				.body(new BadRequestDTO(HttpStatus.BAD_REQUEST.value(),
+						utility.getMessage("forum.message.error.empty.null.field", null),
+						e.getFieldErrors().stream().map(ErrorDTO::new).collect(Collectors.toList())));
 	}
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<GeneralErrorDTO> handleDataIntegrityViolation(DataIntegrityViolationException e) {
-		String message = utility.getMessage("forum.message.error.foreign.key.constraint", null);
-		String description = utility.getCustomErrorMessage(e);
-		GeneralErrorDTO errorDTO = new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(), message, description);
-		return ResponseEntity.badRequest().body(errorDTO);
+		return ResponseEntity.badRequest()
+				.body(new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(),
+						utility.getMessage("forum.message.error.foreign.key.constraint", null),
+						utility.getCustomErrorMessage(e)));
 	}
 
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
 	public ResponseEntity<GeneralErrorDTO> handleMethodArgumentTypeMismatchException(
 			MethodArgumentTypeMismatchException e) {
-		Integer code = HttpStatus.BAD_REQUEST.value();
-		String message = utility.getMessage("forum.message.error.failed.convert.value.type", null);
-		String description = utility.getMessage("forum.message.error.invalid.format", new Object[] { e.getValue() });
-		GeneralErrorDTO badRequest = new GeneralErrorDTO(code, message, description);
-		return ResponseEntity.badRequest().body(badRequest);
+		return ResponseEntity.badRequest()
+				.body(new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(),
+						utility.getMessage("forum.message.error.failed.convert.value.type", null),
+						utility.getMessage("forum.message.error.invalid.format", new Object[] { e.getValue() })));
 	}
 
-	@ExceptionHandler(InvalidFormatException.class)
-	public ResponseEntity<GeneralErrorDTO> handleInvalidFormatException(InvalidFormatException e) {
-		Integer code = HttpStatus.BAD_REQUEST.value();
-		String message = utility.getMessage("forum.message.error.invalid.data.type", null);
-		List<JsonMappingException.Reference> path = e.getPath();
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<GeneralErrorDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
 		StringBuilder pathDescription = new StringBuilder();
-		for (JsonMappingException.Reference reference : path) {
-			if (reference.getFieldName() != null) {
-				pathDescription.append(reference.getFieldName());
+		if (e.getCause() instanceof InvalidFormatException) {
+			InvalidFormatException cause = (InvalidFormatException) e.getCause();
+			for (JsonMappingException.Reference reference : cause.getPath()) {
+				if (reference.getFieldName() != null) {
+					pathDescription.append(reference.getFieldName());
+				}
 			}
 		}
-		String description = utility.getMessage("forum.message.error.invalid.value", new Object[] { pathDescription });
-		GeneralErrorDTO badRequest = new GeneralErrorDTO(code, message, description);
-		return ResponseEntity.badRequest().body(badRequest);
+		return ResponseEntity.badRequest().body(new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(),
+				utility.getMessage("forum.message.error.invalid.data.type", null),
+				utility.getMessage("forum.message.error.invalid.value", new Object[] { pathDescription.toString() })));
 	}
 
 	@ExceptionHandler(PasswordException.class)
-	public ResponseEntity<GeneralErrorDTO> handlePasswordNotMatchException(PasswordException e) {
-		Integer code = HttpStatus.BAD_REQUEST.value();
-		String message = utility.getMessage("forum.message.warn.password", null);
-		String description = e.getMessage();
-		GeneralErrorDTO badRequest = new GeneralErrorDTO(code, message, description);
-		return ResponseEntity.badRequest().body(badRequest);
+	public ResponseEntity<GeneralErrorDTO> handlePasswordException(PasswordException e) {
+		return ResponseEntity.badRequest().body(new GeneralErrorDTO(HttpStatus.BAD_REQUEST.value(),
+				utility.getMessage("forum.message.warn.password", null), e.getMessage()));
 	}
 
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ResponseEntity<GeneralErrorDTO> handleGenericException(Exception e) {
-		Integer code = HttpStatus.INTERNAL_SERVER_ERROR.value();
-		String message = utility.getMessage("forum.message.error.general", null);
-		String description = e.getMessage();
-		GeneralErrorDTO internalServerError = new GeneralErrorDTO(code, message, description);
-		return ResponseEntity.internalServerError().body(internalServerError);
+		return ResponseEntity.internalServerError().body(new GeneralErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				utility.getMessage("forum.message.error.general", null), e.getMessage()));
 	}
 }
